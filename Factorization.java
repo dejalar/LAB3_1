bb68f9d83255e792d2a57ef9a637a31e7f9b5f5a
dejalar <56512549+dejalar@users.noreply.github.com>
2021-05-31 04:57:32 +0300

    Update Factorization.java

--- Factorization.java
+++ Factorization.java
@@ -1,61 +1,89 @@
-package com.lab1a.execution;
+package com.example.lab1_a;
 
-import com.lab1a.utils.exception.TimeExceededException;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
 
-// Факторизація числа методом Ферма
-public class Factorization {
+    private Button getResult;
+    private EditText input;
+    private TextView result;
+    private static int counter = 0;
 
-    private long time0;
 
-    public Long[] factorize(long n) throws TimeExceededException {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
-        this.time0 = System.nanoTime();
-
-        List<Long> multipliers = new ArrayList<>();
-
-        while (n % 2 == 0) {
-
-            multipliers.add(2L);
-            n /= 2;
-
-            long time = System.nanoTime() - this.time0;
-
-            if (time >= 3000000000L) throw new TimeExceededException();
-
-        }
-
-        long[] sqrts = this.getSumOfSquares(n);
-        multipliers.add(Math.abs(sqrts[0] + sqrts[1]));
-        multipliers.add(Math.abs(sqrts[0] - sqrts[1]));
-
-        return multipliers.toArray(new Long[0]);
-
+        Activate();
     }
 
-    private long[] getSumOfSquares(long n) throws TimeExceededException {
 
+    private static long[] GetSumOfSquares(long n) {
         double x, y;
-
+        counter = 0;
         x = Math.ceil(Math.sqrt(n));
         y = Math.pow(x, 2) - n;
 
-        while (Math.sqrt(y) != Math.ceil(Math.sqrt(y))) {
 
+        while (Math.abs(Math.sqrt(y) - Math.ceil(Math.sqrt(y))) > 0.0001f) {
             x++;
             y = Math.pow(x, 2) - n;
-
-            long time = System.nanoTime() - this.time0;
-
-            if (time >= 3000000000L) throw new TimeExceededException();
-
+            counter++;
         }
 
-        return new long[]{(long) x, (long) Math.sqrt(y)};
+        return new long[] {(long) x, (long) Math.sqrt(y)};
+    }
 
+    public void Activate(){
+
+        result = (TextView)findViewById(R.id.textView5);
+        input = (EditText)findViewById(R.id.editTextNumber2);
+        getResult = (Button)findViewById(R.id.button);
+
+        getResult.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        boolean flag = true;
+
+                        int n = Integer.parseInt(input.getText().toString());
+
+                        if (n % 2 == 0) {
+                            result.setText("N має бути непарним");
+                            flag = false;
+                        }
+
+                        if (n <= 1) {
+                            result.setText("N має бути більше 1");
+                            flag = false;
+                        }
+
+                        long[] multipliers = new long[2];
+                        long[] sqrts = GetSumOfSquares(n);
+                        multipliers[0] = Math.abs(sqrts[0] + sqrts[1]);
+                        multipliers[1] = Math.abs(sqrts[0] - sqrts[1]);
+
+                        if (flag) {
+                            String res = String.format(n + " = " + multipliers[0] + "*" + multipliers[1] + "\nКількість ітерацій: " + counter);
+                            result.setText(res);
+
+                        }
+
+                    }
+                }
+        );
     }
 
 }
